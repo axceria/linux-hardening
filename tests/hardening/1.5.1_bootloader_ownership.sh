b68d84437fe4696a6baa4ebffacff8bb138b80bf
Axceria <145180328+axceria@users.noreply.github.com>
2024-03-01 07:14:58 -0800

    Update 1.5.1_bootloader_ownership.sh

--- tests/hardening/1.5.1_bootloader_ownership.sh
+++ tests/hardening/1.5.1_bootloader_ownership.sh
@@ -4,11 +4,12 @@ test_audit() {
     if [ -f "/.dockerenv" ]; then
         skip "SKIPPED on docker"
     else
-        describe Running on blank host
-        register_test retvalshouldbe 0
-        dismiss_count_for_test
+        skip "Skipped DIY"
+        #describe Running on blank host
+        #register_test retvalshouldbe 0
+        #dismiss_count_for_test
         # shellcheck disable=2154
-        run blank "${CIS_CHECKS_DIR}/${script}.sh" --audit-all
+        #run blank "${CIS_CHECKS_DIR}/${script}.sh" --audit-all
 
         # TODO fill comprehensive tests
     fi
